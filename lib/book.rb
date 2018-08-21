5e776c2620da2bad1a5da9320a02f4c1ce9b53a7
Robert Snyder <r.snyder49@gmail.com>
2018-08-21 23:44:58 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
 
+class Book 
+  def initialize(title)
+    @title = title  
+  end 
+  
+  def title 
+    @title
+  end 
+  
+  def author=(name)
+    @author = name 
+  end 
+  
+  def author 
+    @author 
+  end 
+  
+end 
 
+Book.new("And Then There Were None")
