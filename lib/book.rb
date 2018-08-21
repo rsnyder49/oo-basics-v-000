86dc950c7bae760ff5bb5b7a4df1b4483968558d
Robert Snyder <r.snyder49@gmail.com>
2018-08-21 23:53:47 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -37,4 +37,17 @@ class Book
   end
 end 
 
+class Book 
+  attr_accessor :author, :page_count, :genre 
+  attr_reader :title 
+  
+  def initialize(title)
+    @title = title 
+  end 
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end 
+end 
+  
 Book.new("And Then There Were None")
